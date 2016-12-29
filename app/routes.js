d3bfdc12497ca1946d9faccf31177cc082b7d299
Linh Le <linh.le@smoovapp.com>
2016-12-29 16:45:24 +0700

    message logged in user

--- app/routes.js
+++ app/routes.js
@@ -57,6 +57,17 @@ module.exports = function(app, passport) {
 	// =====================================
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/cp_sidebar', isLoggedIn, function(req, res) {
+		res.render('partials/cp_sidebar.ejs', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	// =====================================
+	// CP SECTION =========================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/cp', isLoggedIn, function(req, res) {
 		res.render('cp.ejs', {
 			user : req.user // get the user out of session and pass to template
