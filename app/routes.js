381ffd5feb8e90fd2a079f2c00c7ec812da0f5b1
Linh Le <dev.linhlv@gmail.com>
2017-01-04 20:40:35 +0700

    Redirect to CP after register

--- app/routes.js
+++ app/routes.js
@@ -47,7 +47,7 @@ module.exports = function(app, passport) {
 
 	// process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', // redirect to the secure profile section
+		successRedirect : '/cp', // redirect to the secure profile section
 		failureRedirect : '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
