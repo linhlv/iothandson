5be3b5de6ae628e8feaf99920c2d915dc26eb1b6
Linh Le <linh.le@smoovapp.com>
2017-01-05 10:34:22 +0700

    Add rest validator

--- app/modules/cp.js
+++ app/modules/cp.js
@@ -2,7 +2,8 @@
 // app/routes.js
 module.exports = function(app, passport, isLoggedIn) {
     var express = require('express');
-    var router = express.Router();    
+    var router = express.Router();   
+	var util = require('util'); 
     
 	// =====================================
 	// CP SECTION =========================
@@ -18,11 +19,27 @@ module.exports = function(app, passport, isLoggedIn) {
 	router.post('/connections/', isLoggedIn, function(req, res) {
 		app.get('mongoClient').connect(app.get('dbConnectUrl'), function(err, db){			
 			if(req && req.body){
-				if (err){
-					res.send(err);
-				}else{
-					res.json({ message: 'Connection is created!' });
-				}            	
+				req.checkBody('clientID', 'Client ID is required').notEmpty();
+				req.checkBody('server', 'Server is required').notEmpty();
+				req.checkBody('port', 'Port is required as an integer').notEmpty().isInt();
+				req.checkBody('username', 'Username is required').notEmpty();
+				req.checkBody('password', 'Password is required').notEmpty();
+
+				req.getValidationResult().then(function(result) {
+					if (!result.isEmpty()) {
+						res.send(
+							{
+								message: 'There have been validation errors',
+								errors: result.array()
+							}, 400
+						);
+						return;
+					}
+
+					res.json({
+						clientID: req.body.clientID
+					});
+				});       	
 			}
 		});		
 	});
